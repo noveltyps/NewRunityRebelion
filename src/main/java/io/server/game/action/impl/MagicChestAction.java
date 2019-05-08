61d3ee7661bfea8dd14fb43ba133ddb5b2f87f5b
donvlee97 <43006455+donvlee97@users.noreply.github.com>
2019-05-08 14:33:36 +0200

    push

---
+++ src/main/java/io/server/game/action/impl/MagicChestAction.java
@@ -0,0 +1,64 @@
+package io.server.game.action.impl;
+
+import io.server.content.ActivityLog;
+import io.server.content.CrystalChest;
+import io.server.content.achievement.AchievementHandler;
+import io.server.content.achievement.AchievementKey;
+import io.server.game.Animation;
+import io.server.game.action.Action;
+import io.server.game.action.policy.WalkablePolicy;
+import io.server.game.world.entity.mob.player.Player;
+import io.server.net.packet.out.SendMessage;
+
+/**
+ * Handles opening a crystal chest.
+ *
+ * @author Daniel
+ */
+public final class MagicChestAction extends Action<Player> {
+
+	//private final GameObject object;
+
+	public MagicChestAction(Player player/*, GameObject object*/) {
+		super(player, 1);
+		//this.object = object;
+	}
+
+	@Override
+	protected void onSchedule() {
+		getMob().locking.lock();
+		getMob().inventory.remove(1547, 1);
+		getMob().animate(new Animation(881));
+		getMob().send(new SendMessage("You attempt to unlock the chest..."));
+	}
+
+	@Override
+	public void execute() {
+		cancel();
+	}
+
+	@Override
+	protected void onCancel(boolean logout) {
+		getMob().locking.unlock();
+		getMob().inventory.add(1631, 1);
+		getMob().inventory.add(CrystalChest.getReward());
+		AchievementHandler.activate(getMob(), AchievementKey.MAGIC_CHEST, 1);
+		getMob().send(new SendMessage("...you find a few items inside of the chest."));
+		getMob().activityLogger.add(ActivityLog.MAGIC_CHEST);
+	}
+
+	@Override
+	public String getName() {
+		return "Magic chest";
+	}
+
+	@Override
+	public boolean prioritized() {
+		return false;
+	}
+
+	@Override
+	public WalkablePolicy getWalkablePolicy() {
+		return WalkablePolicy.NON_WALKABLE;
+	}
+}
